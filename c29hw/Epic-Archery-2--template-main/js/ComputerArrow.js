e1ffbc68b4affcf2e8d779b84ebb20c4c628ce78
vikasnkg <96968728+vikasnkg@users.noreply.github.com>
2022-01-10 20:02:25 +0530

    Add files via upload

---
+++ c29hw/Epic-Archery-2--template-main/js/ComputerArrow.js
@@ -0,0 +1,29 @@
+class ComputerArrow {
+  constructor(x, y, width, height, archerAngle) {
+    var options = {
+      restitution: 2,
+      friction: 1.0,
+      density: 1.0,
+      isStatic: true
+    };
+    this.width = width;
+    this.height = height;
+    this.body = Bodies.rectangle(x, y, this.width, this.height, options);
+    Matter.Body.setAngle(this.body, PI / 2); 
+    this.image = loadImage("./assets/arrow.png");
+    World.add(world, this.body);
+ 
+  }
+
+ display() {
+    var pos = this.body.position;
+    var angle = this.body.angle;
+
+    push();
+    translate(pos.x, pos.y);
+    rotate(angle);
+    imageMode(CENTER);
+    image(this.image, 0, 0, this.width, this.height);
+    pop();
+  }
+}
---
+++ c29hw/Epic-Archery-2--template-main/js/back.js
@@ -0,0 +1,25 @@
+class Back {
+    constructor(x, y, width, height) {
+      var options = {
+        isStatic: true
+      };
+  
+      this.body = Bodies.rectangle(x, y, width, height, options);
+  
+      this.width = width;
+      this.height = height;
+      this.image = loadImage("./assets/background.gif");
+  
+          World.add(world, this.body);
+    }
+  
+     display() {
+
+      push();
+      translate(pos.x, pos.y);
+      rotate(angle);
+      imageMode(CENTER);
+      image(this.image, 0, 0, this.width, this.height);
+      pop();
+    }
+  }
